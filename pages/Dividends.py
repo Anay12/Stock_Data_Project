2c4156804104818da0b8558dd2b4bc0fc6faeff0
Anay12 <gandhi52@purdue.edu>
2025-07-19 17:04:12 -0500

    Added dividend page with dividend, eps_trend, quarterly_cashflow, ttm_cashflow methods from yfinance

--- pages/Dividends.py
+++ pages/Dividends.py
@@ -1,3 +1,19 @@
 import streamlit as st
 import yfinance as yf
+import pandas as pd
+import
+
+from pages.Holdings import read_holdings
+
+holdings_df = read_holdings()
+
+for ticker in holdings_df['Ticker']:
+    print(holdings_df.Ticker)
+
+    dividends = ticker.dividends
+
+    eps_trend = ticker.eps_trend
+    quarterly_cashflow = ticker.quarterly_cashflow
+    ttm_cashflow = ticker.ttm_cashflow
+
 
