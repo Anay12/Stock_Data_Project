a0c99c61a00d5835265d526135e3d2250a969a76
Anay12 <gandhi52@purdue.edu>
2025-07-19 17:12:40 -0500

    Added comments and organized + cleaned up code

--- import_stock_data.py
+++ import_stock_data.py
@@ -8,20 +8,30 @@ import matplotlib.pyplot as plt
 import streamlit as st
 from statsmodels.tsa.stattools import adfuller
 
-class StockData():
+# class Data:
+#     def __init__(self):
+#         self.holdings = []
+#
+#     def getHoldings(self):
+#         return self.holdings
+#
+#     def setHoldings(self, holdings):
+#         self.holdings = holdings
+
+
+class Stock:
     def __init__(self, ticker):
         self.ticker = yf.Ticker(ticker)
 
-# access a single ticker and its data
+    # access a single ticker and its data
     def getGeneralInfo(self):
         return self.ticker.info
 
-    def getHoldings(self):
-        return self.ticker.funds_data.top_holdings
-
+    # return past dividends as a Pandas Series
     def getDividends(self):
         return self.ticker.dividends
 
+    # get financial data (income statement)
     def getFinancialData(self, ticker):
         return self.ticker.financials
 
@@ -29,32 +39,54 @@ class StockData():
     def output_financials_to_csv(self, ticker):
         self.getFinancialData('GOOG').to_csv('_'.join([self.ticker.ticker, 'financials.csv']))
 
-
+    # output price information from startDate to endDate to .csv file
     def getPrices(self, startDate, endDate):
         prices_df = yf.download(self.ticker.ticker, startDate, endDate)
         prices_df.to_csv('_'.join([self.ticker, 'prices.csv']))
 
 
 
-# Filter/screen by characteristics or query
-def filter_by_characteristics():
-    response = yf.screen("aggressive_small_caps")
-    print(response)
 
 
-def get_PE_ratio(ticker):
-    yf.Ticker("XLV") # SPDR health care
-    sectorPE = yf.Ticker("XLV").info["trailingPE"]
+class Fund(Stock):
+    def __init__(self, ticker):
+        super().__init__(ticker)
 
-    company_PE_ratios = []
+    # get top holdings for a fund
+    def getHoldings(self):
+        return self.ticker.funds_data.top_holdings
 
-    for holding in yf.Ticker("XLV").funds_data.top_holdings:
-        lookup_holding = yf.Lookup(holding)
-        holding_ticker = lookup_holding.get_stock().to_csv('holdings.csv')
+    def get_sector_PE_ratio(self, ticker):
+        ticker = yf.Ticker("XLV") # SPDR health care
+        sectorPE = ticker.info["trailingPE"]
 
-        company_PE_ratios.append(yf.Ticker(holding_ticker).info["trailingPE"])
+        company_PE_ratios = []
 
-    plt.plot(company_PE_ratios)
+        holdings = self.getHoldings(self.ticker)
+
+        for holding in holdings:
+            lookup_holding = yf.Lookup(holding)
+            holding_ticker = lookup_holding.get_stock().to_csv('holdings_new_df.csv')
+
+            company_PE_ratios.append(yf.Ticker(holding_ticker).info["trailingPE"])
+
+        plt.plot(company_PE_ratios)
+
+
+
+
+
+class OtherMethods:
+    def __init__(self, ticker):
+        self.ticker = yf.Ticker(ticker)
+
+    # Filter/screen by characteristics or query
+    def filter_by_characteristics(self):
+        response = yf.screen("aggressive_small_caps")
+        print(response)
+
+
+
 
 
 # yfinance.Industry('').top_companies
@@ -67,10 +99,15 @@ def get_PE_ratio(ticker):
 
 
 
+
 if __name__ == "__main__":
-    stock = StockData('GOOG')
+    stock = Stock('GOOG')
     stock.output_financials_to_csv('GOOG')
     #get_PE_ratio("XLV")
 
+    st.dataframe('GOOG')
+
+    fund = Fund("XLV")
+    print(fund.getHoldings())
 
 
