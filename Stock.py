003f665c8946ad76b6a7f8703646dd986bce4cac
Anay12 <gandhi52@purdue.edu>
2025-11-26 19:54:52 -0600

    Stock class inherits from Ticker class

--- Stock.py
+++ Stock.py
@@ -5,32 +5,26 @@ import os
 from datetime import datetime
 from datetime import date
 from Functions import date_years_ago, yesterday_date
-
-
 # from Database.models import Holding
 
-class Stock:
-    def __init__(self, ticker_str, quantity=0, holding_type=''):
-        self.ticker_str = ticker_str.upper()
+
+class Stock(yf.Ticker):
+    def __init__(self, ticker, quantity=0, holding_type=''):
+        super().__init__(ticker=ticker)
         self.quantity = quantity
         self.holding_type = holding_type
         self.avg_price = 0
-        self.ticker = yf.Ticker(self.ticker_str)
-
-    def last_price(self):
-        data = self.ticker.history(period="1d")
-        return data["Close"].ilod[-1] if not data.empty else None
 
     def market_value(self):
-        price = self.last_price
-        return None if price is None else price * self.quantity
+        price = self.info.get("regularMarketPrice")
+        return price * self.quantity
 
     def total_gain(self):
-        price = self.last_price
-        return None if price is None else (price-self.avg_price) * self.quantity
+        price = self.info.get("regularMarketPrice")
+        return (price-self.avg_price) * self.quantity
 
     def dividend_yield(self):
-        return self.get_info().get("dividendYield")
+        return self.info.get("dividendYield")
 
     # def to_holding(self):
     #     """ convert to Holding DB object """
@@ -40,37 +34,12 @@ class Stock:
     #
     #     )
 
-    def get_info(self):
-        """ access a single ticker and its data """
-        return self.ticker.info
-
-    def get_dividends(self):
-        """ return past dividends as a Pandas Series """
-        return self.ticker.dividends
-
-    def get_financials(self):
-        """ get financial data (income statement) """
-        return self.ticker.financials
-
     def prices(self, start_date, end_date):
         """ return prices from startDate to endDate in DataFrame """
-        return yf.download(self.ticker_str, start_date, end_date)
-
-    # def output_dividends_to_csv(self):
-    #     path = os.getcwd() + f'/data/dividends_{self.ticker_str}.csv'
-    #     self.get_dividends().to_csv(path)
-    #
-    # def output_financials_to_csv(self):
-    #     """ output financials for 'ticker' to financials_ticker.csv """
-    #     path = os.getcwd() + f'/data/financials_{self.ticker_str}.csv'
-    #     self.get_financials().to_csv(path)
-
-    # def output_prices_to_csv(self):
-    #     path = os.getcwd() + f'/data/prices_{self.ticker_str}.csv'
-    #     self.prices().to_csv(path)
+        return yf.download(self.ticker, start_date, end_date)
 
     def output_to_csv(self, func, df):
-        path = os.getcwd() + f'/data/{func}_{self.ticker_str}.csv'
+        path = os.getcwd() + f'/data/{func}_{self.ticker}.csv'
 
         try:
             match func:
@@ -89,14 +58,15 @@ class Stock:
 
     def get_holding_type(self):
         try:
-            holding_type = self.get_info().get('quoteType', 'Unknown')    # 'EQUITY', 'ETF', 'MUTUALFUND', 'Index'
+            holding_type = self.info.get('quoteType', 'Unknown')    # 'EQUITY', 'ETF', 'MUTUALFUND', 'Index'
         except Exception as e:
-            return f"Could not retrieve holding type for {stock.ticker_str}. Error: {e}"
-
+            return f"Could not retrieve holding type for {self.ticker}. Error: {e}"
         return holding_type
 
     def is_fund(self):
         return self.get_holding_type() != 'EQUITY'
+
+
 
 class Fund(Stock):
     def __init__(self, ticker):
@@ -104,27 +74,28 @@ class Fund(Stock):
 
     def get_top_holdings(self):
         """ get top holdings for this fund """
-        return self.ticker.funds_data.top_holdings
+        return self.funds_data.top_holdings
 
-    def sector_pe_ratio(self):
-        """ Compare sector PE ratio to companies' PE ratios """
-        ticker = yf.Ticker("XLV") # SPDR health care
-        sector_pe = self.ticker.info["trailingPE"]
-
-        company_pe_ratios = []
-
-        holdings = self.get_top_holdings()
-
-        for holding in holdings:
-            lookup_holding = yf.Lookup(holding)
-            holding_ticker = lookup_holding.get_stock().to_csv('holdings_new_df.csv')
-
-            company_pe_ratios.append(yf.Ticker(holding_ticker).info["trailingPE"])
-
-        plt.plot(company_pe_ratios)
 
 
 """ Other Methods """
+
+def sector_pe_ratio(self):
+    """ Compare sector PE ratio to companies' PE ratios """
+    ticker = yf.Ticker("XLV") # SPDR health care
+    sector_pe = self.info["trailingPE"]
+
+    company_pe_ratios = []
+
+    holdings = self.get_top_holdings()
+
+    for holding in holdings:
+        lookup_holding = yf.Lookup(holding)
+        holding_ticker = lookup_holding.get_stock().to_csv('holdings_new_df.csv')
+
+        company_pe_ratios.append(yf.Ticker(holding_ticker).info["trailingPE"])
+
+    plt.plot(company_pe_ratios)
 
 
 def filter_by_characteristics():
@@ -144,7 +115,7 @@ def filter_by_characteristics():
 
 if __name__ == "__main__":
     stock = Stock('GOOG')
-    stock.output_financials_to_csv()
+    stock.output_to_csv('dividends', df=stock.dividends)
     #get_PE_ratio("XLV")
 
     # fund = Fund("XLV")
