75f734099d15205941b65aca342af223ec5621c5
Anay12 <gandhi52@purdue.edu>
2025-12-17 15:27:52 -0600

    Added function to serialize holdings data for json

--- functions.py
+++ functions.py
@@ -5,6 +5,16 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 
 
+def serialize_holding(holding):
+    return {
+        "holding_id": holding.holding_id,
+        "date_added": holding.date_added.strftime('%Y-%m-%d') if holding.date_added else None,
+        "holding_size": holding.holding_size,
+        "purchase_price": holding.purchase_price,
+        "exact_gain": holding.exact_gain if hasattr(holding, "exact_gain") else 0,
+        "percent_gain": holding.percent_gain if hasattr(holding, "percent_gain") else 0
+    }
+
 def date_years_ago(years_ago: int):
     """ return date __ years ago """
     if not isinstance(years_ago, int) or years_ago < 0:
